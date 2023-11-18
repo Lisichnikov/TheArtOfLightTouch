411a9e2d6c21cdd148b5e02169b14acda663c1b6
Алексей Лисичников <alexochanel@gmail.com>
2023-11-18 09:37:19 +0300

    Все работает

--- app/src/main/java/com/example/theartoflighttouch/GameActivity.java
+++ app/src/main/java/com/example/theartoflighttouch/GameActivity.java
@@ -14,12 +14,11 @@ import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
     private static final int MIN_CLICK_INTERVAL = 5000; // В миллисекундах (5 секунд)
-
-private ImageButton player1Button, player2Button;
+private ImageButton player1Button, player2Button, resetButton;
 private TextView player1Text, player2Text, WinText;
+// переменная служит для проверки кто нажал на кнопку
 private boolean isPlayerTurn = true;
 private CountDownTimer turnTimer;
-
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -50,6 +49,9 @@ private CountDownTimer turnTimer;
                 //меняет вторую кнопку на если темную полученное значение равно 1
                 View root2 = findViewById(R.id.button_circle_second);
                 root2.setBackgroundResource(R.drawable.circle_button_dark);
+                //меняет кнопку  заново на светлую если полученное значение равно 2
+                View root3 = findViewById(R.id.reset_button);
+                root3.setBackgroundResource(R.drawable.button_reset);
 
                 // меняет на темную тему если полученное значение равно 2
             } else {
@@ -62,7 +64,11 @@ private CountDownTimer turnTimer;
                 //меняет вторую кнопку на светлую если полученное значение равно 2
                 View root2 = findViewById(R.id.button_circle_second);
                 root2.setBackgroundResource(R.drawable.circle_button_white);
+                //меняет кнопку  заново на светлую если полученное значение равно 2
+                View root3 = findViewById(R.id.reset_button);
+                root3.setBackgroundResource(R.drawable.button_reset_white);
             }
+            //обработка нажатия кнопки первого игрока
             player1Button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -72,6 +78,7 @@ private CountDownTimer turnTimer;
                     startTurnTimer();
                 }
             });
+            //обработка нажатия кнопки второго игрока
             player2Button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -82,11 +89,18 @@ private CountDownTimer turnTimer;
                 }
             });
         }
+        public void onClickButtonReset(View view){
+            WinText.setText("");
+            player1Button.setEnabled(true);
+            player2Button.setEnabled(true);
+        }
+        //метод для создания рандомного числа
         private void generateRandomNumber(TextView playerNumberField) {
             Random random = new Random();
             int randomNumber = random.nextInt(20) + 1;
             playerNumberField.setText(String.valueOf(randomNumber));
         }
+        //метод для проверки числа на соответствие
         private void checkWinner() {
             String player1Number = player1Text.getText().toString();
             String player2Number = player2Text.getText().toString();
@@ -97,9 +111,12 @@ private CountDownTimer turnTimer;
                 } else {
                     WinText.setText("Выиграл первый игрок!");
                 }
+                player1Button.setEnabled(false);
+                player2Button.setEnabled(false);
                 resetGame();
             }
         }
+        // метод создания таймера
         private void startTurnTimer() {
             if (turnTimer != null) {
                 turnTimer.cancel();
@@ -115,10 +132,13 @@ private CountDownTimer turnTimer;
                     } else {
                         WinText.setText("Выиграл первый игрок!");
                     }
+                    player1Button.setEnabled(false);
+                    player2Button.setEnabled(false);
                     resetGame();
                 }
             }.start();
         }
+        // метод для перезапуска игры
         private void resetGame() {
             if (turnTimer != null) {
                 turnTimer.cancel();
